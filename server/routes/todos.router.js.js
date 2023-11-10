d7ea0cfb76b6097799595d580a0d42bf83dabc34
Marcos Andrade <mnandrade1999@gmail.com>
2023-11-09 20:01:02 -0600

    all server side code and functions working tested with postman

--- server/routes/todos.router.js.js
+++ server/routes/todos.router.js.js
@@ -4,28 +4,97 @@ const pool = require('../modules/pool');
 
 // GET all Tasks
 
-router.get(()=>{
-    let queryText; // = SELECT FROM TABLE
+router.get('/',(req,res) => {
+    let queryText = 'SELECT * FROM "todos" ORDER BY "id";'
     pool.query(queryText)
-    .then(result =>{
-        res.send(); //<--- inside should be result.rows, should return results in an object
+    .then((dbResult) =>{
+        console.log('dbResult.rows:', dbResult.rows);
+        res.send(dbResult.rows); //<--- inside should be result.rows, should return results in an object
     })
-    .catch(error =>{
-        console.log("error getting ToDoList", error);
+    .catch(dbError =>{
+        console.log("error getting ToDoList", dbError);
         res.sendStatus(500);
     })
-    
+
 });
 
+
 // // gives ability to create a new todo item
-// router.post()
+router.post('/',(req,res) => {
+    let queryText = 
+    `
+    INSERT INTO "todos"
+    ("text","isComplete")
+    VALUES
+    ($1,$2);
+    `
+
+
+
+    const sqlValues = [
+        req.body.text,
+        req.body.isComplete
+    ]
+
+    pool.query(queryText,sqlValues)
+    .then((dbResult) => {
+        res.sendStatus(201)
+    })
+    .catch((dbError)=>{
+        console.log('POST / todos query failed', dbError)
+    })
+});
 
 
 // // for updating table: example updating isCompleted status
 // router.put()
+router.put('/:id',(req,res) => {
+
+let idOfTask = req.params.id;
+
+const sqlText = 
+`
+UPDATE "todos"
+SET "isComplete" = NOT "isComplete"
+WHERE "id" = ($1)
+`
+
+const sqlValues = [idOfTask];
+
+pool.query(sqlText,sqlValues)
+.then((dbResult) => {
+    res.sendStatus(200);
+    })
+.catch((dbError) =>{
+    console.log('PUT /koalas:id failed',dbError)
+    res.sendStatus(500);
+    })
+});
+
+
+
 
 // // for deleting task from task list and from database 
 // router.delete()
+router.delete('/:id',(req,res)=>{
+
+    let idOfTaskToDelete = req.params.id;
+    const sqlText = 
+    `
+    DELETE FROM "todos"
+        WHERE "id" = ($1)
+    `
+    const sqlValues = [idOfTaskToDelete];
+
+    pool.query(sqlText,sqlValues)
+    .then((dbResult) => {
+        res.sendStatus(200);
+    })
+    .catch((dbError) => {
+        console.log("DELETE /todos/:id failed", dbError);
+        res.sendStatus(500);
+    })
+});
 
 
 
